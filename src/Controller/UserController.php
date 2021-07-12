371091edf6ca1151b163de49cb45e4eba6091449
Matthias LEROUX <matthiasleroux@laposte.net>
2021-07-12 13:41:20 +0200

    Create a user and persist in the database, then redirect to the users list wit a success message.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,15 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -14,17 +20,35 @@ class UserController extends AbstractController
     /**
      * @Route("/users/list", name="user_list")
      */
-    public function list()
+    public function list(UserRepository $userRepository): Response
     {
-        return $this->render('user/list.html.twig');
+        return $this->render('user/list.html.twig', ['users' => $userRepository->findAll()]);
     }
 
     /**
      * @Route("/users/create", name="user_create")
      */
-    public function create()
+    public function create(EntityManagerInterface $em, Request $request, UserPasswordHasherInterface $passwordHasher) : Response
     {
-        $form = $this->createForm(UserType::class);
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'L\'utilisateur a été créé');
+
+            return $this->redirectToRoute('user_list');
+        }
 
         return $this->render('user/create.html.twig', [
             'formView' => $form->createView()
