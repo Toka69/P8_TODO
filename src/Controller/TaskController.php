adb568ecb677cd926b78b590daa18ca0659e895b
Matthias LEROUX <matthiasleroux@laposte.net>
2021-07-13 10:21:41 +0200

    Complete list and create task.

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,30 +4,47 @@
 namespace App\Controller;
 
 
+use App\Entity\Task;
 use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class TaskController extends AbstractController
 {
     /**
      * @Route("/tasks", name="task_list")
      */
-    public function list()
+    public function list(TaskRepository $taskRepository)
     {
-        return $this->render('task/list.html.twig');
+        return $this->render('task/list.html.twig', ['tasks' => $taskRepository->findBy(['user' => $this->getUser()])]);
     }
 
     /**
      * @Route("/tasks/create", name="task_create")
      */
-    public function create()
+    public function create(Request $request, EntityManagerInterface $entityManager)
     {
-        $form = $this->createForm(TaskType::class);
-        return $this->render('task/create.html.twig', [
-            'form' => $form->createView()
-                ]
-        );
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setIsDone(false)
+                ->setUser($this->getUser());
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
     }
 
     /**
