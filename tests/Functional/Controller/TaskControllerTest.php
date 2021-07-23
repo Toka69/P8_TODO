2b77e2a18f6fa0ba127d829080bb9a3f47c89e40
Matthias LEROUX <matthiasleroux@laposte.net>
2021-07-23 20:57:43 +0200

    Check the content of the tasks pages "to do" and "is done".

--- tests/Functional/Controller/TaskControllerTest.php
+++ tests/Functional/Controller/TaskControllerTest.php
@@ -60,11 +60,11 @@ class TaskControllerTest extends WebTestCase
     }
 
     /**
-     * Check if the displayed tasks belong to this owner.
+     * Check if the displayed tasks are only "to do" and that they belong to its owner.
      */
     public function testDisplayedTasksList(): void
     {
-        $tasks = $this->entityManager->getRepository(Task::class)->findBy(['user' => $this->testUser]);
+        $tasks = $this->entityManager->getRepository(Task::class)->findBy(['user' => $this->testUser, 'isDone' => false]);
         $tasksId = [];
         foreach ($tasks as $task)
         {
@@ -107,6 +107,57 @@ class TaskControllerTest extends WebTestCase
 
         $this->assertStringContainsString('1', (string)$test);
     }
+
+    /**
+     * Check if the displayed tasks are only "is done" and that they belong to its owner.
+     */
+    public function testDisplayedIsDoneTasksList(): void
+    {
+        $tasks = $this->entityManager->getRepository(Task::class)->findBy(['user' => $this->testUser, 'isDone' => true]);
+        $tasksId = [];
+        foreach ($tasks as $task)
+        {
+            array_push($tasksId, $task->getId());
+        }
+
+        $crawler = $this->client->request('GET', '/tasks/done');
+
+        $nodeValues = $crawler->filter('h4 > a')->each(function ($node, $i) {
+            return $node->attr('href');
+        });
+
+        $tasksListTest = [];
+        $test = false;
+        foreach($nodeValues as $nodeValue)
+        {
+            $result = [];
+            $nodeId = strstr(strstr($nodeValue, 'tasks/'), '/edit', true);
+            for($i=0; $i < count($tasksId); $i++){
+                $nodeContain = false;
+                if(str_contains($nodeId, $tasksId[$i]))
+                {
+                    $nodeContain = true;
+                }
+                array_push($result, $nodeContain);
+            }
+
+            $arrayContain = false;
+            if (in_array(true, $result))
+            {
+                $arrayContain = true;
+            }
+            array_push($tasksListTest, $arrayContain);
+        }
+
+        if (!in_array(false, $tasksListTest))
+        {
+            $test = true;
+        }
+
+        $this->assertStringContainsString('1', (string)$test);
+    }
+
+
 
     protected function tearDown(): void
     {
