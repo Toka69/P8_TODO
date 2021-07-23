0ff2a2b3b565feafa36350829f25fc576b56533f
Matthias LEROUX <matthiasleroux@laposte.net>
2021-07-23 14:14:02 +0200

    Move user connection inside setUp().

--- tests/Functional/Controller/TaskControllerTest.php
+++ tests/Functional/Controller/TaskControllerTest.php
@@ -3,37 +3,62 @@
 namespace App\Tests\Functional\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskControllerTest extends WebTestCase
 {
+    /**
+     * @var EntityManager
+     */
     private $entityManager;
 
+    /**
+     * @var KernelBrowser
+     */
     private $client;
 
+    /**
+     * Set Up to load kernel and user
+     */
     protected function setUp(): void
     {
         $this->client = $this->createClient();
         $this->entityManager = $this->client->getContainer()
             ->get('doctrine')
             ->getManager();
-    }
 
-    public function tasksListTest(): void
-    {
         $testUser = $this->entityManager->getRepository(User::class)->findOneBy(['username' => 'user']);
         $this->client->loginUser($testUser);
-        $crawler = $this->client->request('GET', '/tasks');
+    }
+
+    /**
+     * Login and access tasks list
+     */
+    public function testTasksList(): void
+    {
+        $crawler = $this->client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame('200');
+    }
 
+    /**
+     * Login and access done tasks list
+     */
+    public function testTasksListIsDone(): void
+    {
+        $crawler = $this->client->request('GET', '/tasks/done');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame('200');
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
 
-        // doing this is recommended to avoid memory leaks
         $this->entityManager->close();
         $this->entityManager = null;
     }
