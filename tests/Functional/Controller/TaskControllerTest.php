f3a15f9bed17e3965b08eb09fa9ef2e56ea62695
Matthias LEROUX <matthiasleroux@laposte.net>
2021-07-23 20:30:15 +0200

    Remove the "status code" assertion because "is successful" is the same result, and add the new test to check if the displayed tasks belong to this owner.

--- tests/Functional/Controller/TaskControllerTest.php
+++ tests/Functional/Controller/TaskControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional\Controller;
 
+use App\Entity\Task;
 use App\Entity\User;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
@@ -20,6 +21,11 @@ class TaskControllerTest extends WebTestCase
     private $client;
 
     /**
+     * @var User
+     */
+    private $testUser;
+
+    /**
      * Set Up to load kernel and user
      */
     protected function setUp(): void
@@ -29,8 +35,8 @@ class TaskControllerTest extends WebTestCase
             ->get('doctrine')
             ->getManager();
 
-        $testUser = $this->entityManager->getRepository(User::class)->findOneBy(['username' => 'user']);
-        $this->client->loginUser($testUser);
+        $this->testUser = $this->entityManager->getRepository(User::class)->findOneBy(['username' => 'user']);
+        $this->client->loginUser($this->testUser);
     }
 
     /**
@@ -38,10 +44,9 @@ class TaskControllerTest extends WebTestCase
      */
     public function testTasksList(): void
     {
-        $crawler = $this->client->request('GET', '/');
+        $this->client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertResponseStatusCodeSame('200');
     }
 
     /**
@@ -49,10 +54,58 @@ class TaskControllerTest extends WebTestCase
      */
     public function testTasksListIsDone(): void
     {
-        $crawler = $this->client->request('GET', '/tasks/done');
+        $this->client->request('GET', '/tasks/done');
 
         $this->assertResponseIsSuccessful();
-        $this->assertResponseStatusCodeSame('200');
+    }
+
+    /**
+     * Check if the displayed tasks belong to this owner.
+     */
+    public function testDisplayedTasksList(): void
+    {
+        $tasks = $this->entityManager->getRepository(Task::class)->findBy(['user' => $this->testUser]);
+        $tasksId = [];
+        foreach ($tasks as $task)
+        {
+            array_push($tasksId, $task->getId());
+        }
+
+        $crawler = $this->client->request('GET', '/tasks');
+
+        $nodeValues = $crawler->filter('h4 > a')->each(function ($node, $i) {
+            return $node->attr('href');
+        });
+
+        $tasksListTest = [];
+        $test = false;
+        foreach($nodeValues as $nodeValue)
+        {
+            $result = [];
+            $nodeId = strstr(strstr($nodeValue, 'tasks/'), '/edit', true);
+            for($i=0; $i < count($tasksId); $i++){
+                $nodeContain = false;
+                if(str_contains($nodeId, $tasksId[$i]))
+                {
+                    $nodeContain = true;
+                }
+                array_push($result, $nodeContain);
+            }
+
+            $arrayContain = false;
+            if (in_array(true, $result))
+            {
+                $arrayContain = true;
+            }
+            array_push($tasksListTest, $arrayContain);
+        }
+
+        if (!in_array(false, $tasksListTest))
+        {
+            $test = true;
+        }
+
+        $this->assertStringContainsString('1', (string)$test);
     }
 
     protected function tearDown(): void
