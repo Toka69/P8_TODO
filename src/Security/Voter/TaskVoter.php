2bc3f2b3150b38787b6d32b786cde6e4a732793a
Matthias LEROUX <matthiasleroux@laposte.net>
2021-07-27 10:42:02 +0200

    Check Voters.

--- src/Security/Voter/TaskVoter.php
+++ src/Security/Voter/TaskVoter.php
@@ -11,14 +11,24 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class TaskVoter extends Voter
 {
-
+    /**
+     * @param string $attribute
+     * @param mixed $subject
+     * @return bool
+     */
     protected function supports(string $attribute, $subject): bool
     {
         return in_array($attribute, ['EDIT', 'TOGGLE', 'DELETE'])
             && $subject instanceof Task;
     }
 
-    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token)
+    /**
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     * @return bool
+     */
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
     {
         $user = $token->getUser();
 
