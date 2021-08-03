f5064f6116cb4aa3d4f95568bd24521f91f05bfb
Matthias LEROUX <matthiasleroux@laposte.net>
2021-08-03 17:11:16 +0200

    Clear cache when login event.

---
+++ src/EventListener/LoginListener.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\EventListener;
+
+
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+class LoginListener
+{
+    protected $cache;
+
+    public function __construct(AdapterInterface $cache){
+        $this->cache = $cache;
+    }
+
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event){
+        if($event->getRequest()->attributes->get('_route') == "login_check") {
+            $this->cache->clear();
+        }
+    }
+}
