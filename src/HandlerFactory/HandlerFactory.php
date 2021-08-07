cdd59b647ef7f851cb3459737a2e106bf4844def
Matthias LEROUX <matthiasleroux@laposte.net>
2021-08-07 18:36:51 +0200

    Container of services, CompilerPass, abstract classes and interface for factoring.

---
+++ src/HandlerFactory/HandlerFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\HandlerFactory;
+
+
+ use Psr\Container\ContainerInterface;
+
+ /**
+  * Class HandlerFactory
+  * @package App\HandlerFactory
+  */
+class HandlerFactory implements HandlerFactoryInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected ContainerInterface $container;
+
+    /**
+     * HandlerFactory constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param string $handler
+     * @return HandlerInterface
+     */
+     public function createHandler(string $handler): HandlerInterface
+     {
+         return $this->container->get($handler);
+     }
+}
---
+++ src/HandlerFactory/HandlerFactoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\HandlerFactory;
+
+/**
+ * Interface HandlerFactoryInterface
+ * @package App\HandlerFactory
+ */
+interface HandlerFactoryInterface
+{
+    /**
+     * @param string $handler
+     * @return HandlerInterface
+     */
+    public function createHandler(string $handler): HandlerInterface;
+}
