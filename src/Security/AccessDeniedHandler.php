0e37eaf5e32c3e82a73605681c08324faccd836d
Matthias LEROUX <matthiasleroux@laposte.net>
2021-08-15 15:02:44 +0200

    Add an error Handler.

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Security;
+
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private UrlGeneratorInterface $urlGenerator;
+    protected SessionInterface $session;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator, SessionInterface $session)
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->session = $session;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException){
+        $this->session->getFlashBag()->add('error', $accessDeniedException->getMessage());
+
+        return new RedirectResponse($request->headers->get('referer'));
+    }
+}
